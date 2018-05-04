f1ea1f69f890b95ba572e6fd9da292390d2846b0
taekimm <taekimm@gmail.com>
2018-05-03 17:01:46 -0700

    done without redux, to add search on main page

---
+++ js/Search.jsx
@@ -0,0 +1,41 @@
+// @flow
+
+import React, { Component } from 'react';
+import ShowCard from './ShowCard';
+import Header from './Header';
+
+class Search extends Component {
+    state = {
+        searchTerm: ''
+    };
+    props: {
+        shows: Array<Show>
+    };
+    handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
+        this.setState({ searchTerm: event.target.value });
+    };
+
+    render() {
+        return (
+            <div className="search">
+                <Header
+                    showSearch
+                    searchTerm={this.state.searchTerm}
+                    handleSearchTermChange={this.handleSearchTermChange}
+                />
+                <div>
+                    {this.props.shows
+                        .filter(
+                            show =>
+                                `${show.title} ${show.description}`
+                                    .toUpperCase()
+                                    .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+                        )
+                        .map(show => <ShowCard key={show.imdbID} {...show} />)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Search;
